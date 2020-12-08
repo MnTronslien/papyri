09963a1a1593902e6d3ef52197b3a03db46a5c04
Mattias T <mattias@blackbook.no>
2020-12-08 18:07:15 +0100

    Added some usefull Batch files
    first time setup, for running and setting up the server

---
+++ FirstTime.bat
@@ -0,0 +1,3 @@
+#@ECHO OFF
+Python papyri.py --world D:\Documents\Minecraft\BukkitServer\Westerdale_one --type java --output D:\Documents\Minecraft\PapyrusServerMap\Output --copytemplate --debug
+pause
---
+++ ScheduledUpdateMap.bat
@@ -0,0 +1,6 @@
+::Batch file that rundt inthe background to update the papyrus map every 30 minutes. 
+:loop
+Python papyri.py --world D:\Documents\Minecraft\BukkitServer\Westerdale_one --type java --output D:\Documents\Minecraft\PapyrusServerMap\Output
+::pause the terminal for 30 minutes whether you press a key or not:
+timeout /t 1800 /nobreak
+goto loop
