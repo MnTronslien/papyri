a3d479b1fd9a7507ccdfca99a72e36ea71d14829
Mattias T <mattias@blackbook.no>
2020-12-14 05:22:29 +0100

    Update to Bat files

--- ScheduledUpdateMap.bat
+++ ScheduledUpdateMap.bat
@@ -2,5 +2,5 @@
 :loop
 Python papyri.py --world D:\Documents\Minecraft\BukkitServer\Westerdale_one --type java --output D:\Documents\Minecraft\PapyrusServerMap\Output
 ::pause the terminal for 30 minutes whether you press a key or not:
-timeout /t 1800 /nobreak
+timeout /t 1800
 goto loop
